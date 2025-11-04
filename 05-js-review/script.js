d74f0977271d85e7f66c43e10975e5e5fbfab30f
Alparslan Tarkan <atarkan@hotmail.com>
2025-11-04 09:12:16 +0100

    the array filter method

--- 05-js-review/script.js
+++ 05-js-review/script.js
@@ -225,3 +225,13 @@ const essentialData = books.map((book) => ({
   reviewsCount: getTotalReviewCount(book),
 }));
 essentialData;
+
+const longBooks = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooks;
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+
+adventureBooks;
