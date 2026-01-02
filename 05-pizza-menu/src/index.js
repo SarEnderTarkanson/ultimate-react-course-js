5d66d376e82aa11ac91102ea7e72b6f0d1ccbed4
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-02 18:37:31 +0100

    rendering lists

--- 05-pizza-menu/src/index.js
+++ 05-pizza-menu/src/index.js
@@ -69,7 +69,17 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza
+            key={pizza.name}
+            pizzaObj={pizza}
+          />
+        ))}
+      </ul>
+
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -80,19 +90,20 @@ function Menu() {
         ingredients="Tomato, mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />
+      /> */}
     </main>
   );
 }
 
 function Pizza(props) {
+  console.log(props);
   return (
     <div className="pizza">
-      <img src={props.photoName} alt="Pizza Spinaci" />
+      <img src={props.pizzaObj.photoName} alt="Pizza Spinaci" />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ngredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
     </div>
   );
